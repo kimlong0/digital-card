8c16a4311a0e1baad331161fd3448620fa00107d
kimlong0 <kimlong0@uw.edu>
2022-08-12 13:34:49 -0700

    fix GitHub img filepath

--- src/Links.jsx
+++ src/Links.jsx
@@ -2,7 +2,7 @@ export default function Links() {
     return(
         <div className="link-containers">
             <a href="https://twitter.com/0xKimlong" target="_blank" className="social-logo"><img src="Twitter Icon.svg" alt="twitter logo"/></a>
-            <a href="https://github.com/kimlong0" target="_blank" className="social-logo"><img src="Github Icon.svg" alt="github logo"/></a>
+            <a href="https://github.com/kimlong0" target="_blank" className="social-logo"><img src="GitHub Icon.svg" alt="github logo"/></a>
         </div>
     )
 }
