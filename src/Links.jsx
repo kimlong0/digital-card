31a57991faf7c36af206de28c0d2ae533a019b70
kimlong0 <kimlong0@uw.edu>
2022-08-12 00:39:39 -0700

    deployment attempt 4

--- src/Links.jsx
+++ src/Links.jsx
@@ -1,8 +1,8 @@
 export default function Links() {
     return(
         <div className="link-containers">
-            <a href="https://twitter.com/0xKimlong" target="_blank" className="social-logo"><img src="./src/assets/Twitter Icon.svg" alt="twitter logo"/></a>
-            <a href="https://github.com/kimlong0" target="_blank" className="social-logo"><img src="./src/assets/github-icon.svg" alt="github logo"/></a>
+            <a href="https://twitter.com/0xKimlong" target="_blank" className="social-logo"><img src="./dist/assets/Twitter Icon.svg" alt="twitter logo"/></a>
+            <a href="https://github.com/kimlong0" target="_blank" className="social-logo"><img src="./dist/assets/github-icon.svg" alt="github logo"/></a>
         </div>
     )
 }
